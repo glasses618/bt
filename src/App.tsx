44becb10a421267607f98630711211f91c1d054a
edwardlai3582@gmail.com <edwardlai3582@gmail.com>
2020-12-10 18:43:03 +0800

    z

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from 'react';
+// import { fabric } from 'fabric';
+
+interface RectProps { ctx: any, x: any, y: any, width: any, height: any, }
+
+function rect(props: RectProps) {
+  const {ctx, x, y, width, height} = props;
+  ctx.fillRect(x, y, width, height);
+}
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    function updateCanvas() {
+      const ctx = canvasRef.current?.getContext('2d');
+      if (ctx) {
+        ctx.clearRect(0,0, 300, 300);
+        // draw children “components”
+        rect({ctx, x: 10, y: 10, width: 50, height: 50});
+        rect({ctx, x: 110, y: 110, width: 50, height: 50});
+      }
+    }
+
+    updateCanvas();
+  }, [canvasRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor:'teal'}}>
+      <canvas ref={canvasRef} width={600} height={600}/>
     </div>
   );
 }
